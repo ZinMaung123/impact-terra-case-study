de8dc5205670f115516502c488a81ef168e16f38
Zin Maung Maung <zinmaungmaung.utycc@gmail.com>
2020-05-27 21:32:26 +0630

    create market

---
+++ app/Market.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Market extends Model
+{
+    use SoftDeletes;
+
+    protected $table = "markets";
+
+    protected $fillable = [
+        "name", "description"
+    ];
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+}
